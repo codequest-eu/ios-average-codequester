4e8326dda82666d318bc190d2ec6b3a9e739f03c
Hanna <dutkiewicz.hanna@gmail.com>
2019-08-01 12:18:56 +0200

    Changed gitignore

--- ios-avarage-codequester/faceBlendCommon.hpp
+++ ios-avarage-codequester/faceBlendCommon.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5cfbde8fd1d3ecbc92156907343f36acb3ed518d7bebb4b15f9101c52846fb1
-size 9903
+oid sha256:73d64c69309045dcddc0016c0d451cbe06fc9adbc7a87abd2b260701758baf47
+size 9710
