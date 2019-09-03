5c302c2c65e77e13731b806ead8a1d280e01dd57
Hanna <dutkiewicz.hanna@gmail.com>
2019-09-03 19:40:28 +0200

    Beautify faceBlendCommon methods and added disclaimer

--- ios-avarage-codequester/faceBlendCommon.hpp
+++ ios-avarage-codequester/faceBlendCommon.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0689bf44b9a4c9d32e533eee6dc25942dfb3b032798c55b315f28e76107d9e95
-size 10845
+oid sha256:61396f147715606caeb4ea421b212b68f244904aed4d6adf003b6aea99666c23
+size 11188
