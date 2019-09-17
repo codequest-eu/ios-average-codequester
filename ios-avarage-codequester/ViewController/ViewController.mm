99b5817294bebd97201481f1b1bcc2e347244164
Hanna <dutkiewicz.hanna@gmail.com>
2019-09-17 21:39:25 +0200

    Optimize by creating more vectors at start

--- ios-avarage-codequester/ViewController/ViewController.mm
+++ ios-avarage-codequester/ViewController/ViewController.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d51dafb52c2e94db4a03dc509da4a016dcd132756bec45eac30dc82f6a1cec5
-size 8550
+oid sha256:3c909dd899b83e43de6fc59c768fab79719e500310ff8f124b7419e7fd554427
+size 8855
