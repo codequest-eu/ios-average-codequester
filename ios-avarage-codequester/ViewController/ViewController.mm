e2f778b89123528f4f36948e4092448ac332cfc3
Hanna <dutkiewicz.hanna@gmail.com>
2019-09-17 21:55:11 +0200

    Cleaning up and refactoring

--- ios-avarage-codequester/ViewController/ViewController.mm
+++ ios-avarage-codequester/ViewController/ViewController.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c909dd899b83e43de6fc59c768fab79719e500310ff8f124b7419e7fd554427
-size 8855
+oid sha256:fa5de72ec836d53408b7199ebb35048363f7e385c81b4c58e4c91320872d3f4d
+size 9081
