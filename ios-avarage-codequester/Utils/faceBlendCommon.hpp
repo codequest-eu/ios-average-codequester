5f5219ba3c6877a122d06d3ca95a00c3bb279e4b
Hanna <dutkiewicz.hanna@gmail.com>
2019-09-17 22:20:45 +0200

    Clean utils comments

--- ios-avarage-codequester/Utils/faceBlendCommon.hpp
+++ ios-avarage-codequester/Utils/faceBlendCommon.hpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4a759d24e74f0067ab6024ef1747ecb42aee8c799641a5dc756189e6d6a6376
-size 10742
+oid sha256:855b7f774fae841e5f0cb90aa90ad7922edecf7d71ca476eb4553fca96e8bbc8
+size 10650
--- ios-avarage-codequester/ViewController/ViewController.mm
+++ ios-avarage-codequester/ViewController/ViewController.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa5de72ec836d53408b7199ebb35048363f7e385c81b4c58e4c91320872d3f4d
-size 9081
+oid sha256:8080963294c654b5966260e8dc90c4120bedd6ea8b7a46005ae5cc1fbbc8fb5d
+size 9134
