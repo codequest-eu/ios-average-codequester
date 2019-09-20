871c36bbda114e2b23987976e3fbe81bd1f7a80e
Hanna <hanna.dutkiewicz@codequest.com>
2019-09-20 15:17:36 +0200

    Remove Pods from project

--- Podfile
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0828a760eadfaa7858c7fbe7e5e0e0a03763327510c29f1e1c49e4daa1723845
-size 284
--- Pods/Target Support Files/Pods-ios-avarage-codequester/Pods-ios-avarage-codequester-dummy.m
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:42fdfb2b7a4b6c272af88834f91a11ca7be32bd4321ec5e4063cd5f6093cd524
-size 160
